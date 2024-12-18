5238545570c18de4b665f7cfc02e514194869341
Herman <palchukgerman@gmail.com>
2024-12-18 21:16:22 +0300

    Cleared config.py

---
+++ lab9/config.py
@@ -0,0 +1,7 @@
+class Config:
+    DEBUG = True
+    SECRET_KEY = 'xxx'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'xxx'
+    
